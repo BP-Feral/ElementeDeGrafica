7dbd19ebd258249ba131e3f48f2c12e181273625
BlackPigeon <mihai.pricob@yahoo.com>
2023-10-22 21:39:40 +0300

    Update Window3D.cs

--- Pricob/Window3D.cs
+++ Pricob/Window3D.cs
@@ -9,6 +9,12 @@ namespace Pricob
 {
     internal class Window3D : GameWindow
     {
+        // Valori Cub laboratorul 2
+        private float cubeSize = 2.0f;
+        private Vector3 cubePosition = Vector3.Zero;
+        
+        private MouseState prevMouseState;
+
         public Window3D() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
         {
             VSync = VSyncMode.On;
@@ -26,6 +32,8 @@ namespace Pricob
 
 
             GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+
+            prevMouseState = Mouse.GetState();
         }
 
         protected override void OnResize(EventArgs e)
@@ -37,6 +45,7 @@ namespace Pricob
 
             // Setare Perspectiva
             Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 256);
+            GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref perspective);
 
             // Setare Camera
@@ -58,6 +67,53 @@ namespace Pricob
                 Exit();
             }
 
+            // Miscare cub cu mouse Laboratorul 2
+            int deltaX = thisMosue.X - prevMouseState.X;
+            int deltaY = thisMosue.Y - prevMouseState.Y;
+
+            cubePosition.X += deltaX * 0.1f;
+            cubePosition.Y -= deltaY * 0.1f;
+
+            prevMouseState = thisMosue;
+
+            // Miscare cub cu tastatura Laboratorul 2
+            float moveSpeed = 1.0f;
+            if (thisKeyboard[Key.W])
+            {
+                cubePosition.Z -= moveSpeed;
+            }
+
+            if (thisKeyboard[Key.S])
+            {
+                cubePosition.Z += moveSpeed;
+            }
+
+            if (thisKeyboard[Key.A])
+            {
+                cubePosition.X -= moveSpeed;
+            }
+
+            if (thisKeyboard[Key.D])
+            {
+                cubePosition.X += moveSpeed;
+            }
+
+            if (thisKeyboard[Key.Q])
+            {
+                cubePosition.Y -= moveSpeed;
+            }
+
+            if (thisKeyboard[Key.E])
+            {
+                cubePosition.Y += moveSpeed;
+            }
+
+            if (thisKeyboard[Key.R])
+            {
+                cubePosition = Vector3.Zero;
+            }
+
+            // Meniu ajutor
             if (thisKeyboard[Key.H])
             {
                 displayHelp();
@@ -71,16 +127,83 @@ namespace Pricob
             GL.Clear(ClearBufferMask.ColorBufferBit);
             GL.Clear(ClearBufferMask.DepthBufferBit);
 
-            // Cod Render
+            ///
+            /// Cod Render
+            ///
+            
+            // Render Cube laboratorul 2
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.PushMatrix();
+            GL.Translate(cubePosition);
+            RenderCube();
+            GL.PopMatrix();
 
             SwapBuffers();
         }
+
+        private void RenderCube()
+        {
+            GL.Begin(PrimitiveType.Quads);
+
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-cubeSize, -cubeSize, -cubeSize);
+            GL.Vertex3(-cubeSize, cubeSize, -cubeSize);
+            GL.Vertex3(cubeSize, cubeSize, -cubeSize);
+            GL.Vertex3(cubeSize, -cubeSize, -cubeSize);
+
+            GL.Color3(Color.Honeydew);
+            GL.Vertex3(-cubeSize, -cubeSize, -cubeSize);
+            GL.Vertex3(cubeSize, -cubeSize, -cubeSize);
+            GL.Vertex3(cubeSize, -cubeSize, cubeSize);
+            GL.Vertex3(-cubeSize, -cubeSize, cubeSize);
+
+            GL.Color3(Color.Moccasin);
+
+            GL.Vertex3(-cubeSize, -cubeSize, -cubeSize);
+            GL.Vertex3(-cubeSize, -cubeSize, cubeSize);
+            GL.Vertex3(-cubeSize, cubeSize, cubeSize);
+            GL.Vertex3(-cubeSize, cubeSize, -cubeSize);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-cubeSize, -cubeSize, cubeSize);
+            GL.Vertex3(cubeSize, -cubeSize, cubeSize);
+            GL.Vertex3(cubeSize, cubeSize, cubeSize);
+            GL.Vertex3(-cubeSize, cubeSize, cubeSize);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-cubeSize, cubeSize, -cubeSize);
+            GL.Vertex3(-cubeSize, cubeSize, cubeSize);
+            GL.Vertex3(cubeSize, cubeSize, cubeSize);
+            GL.Vertex3(cubeSize, cubeSize, -cubeSize);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(cubeSize, -cubeSize, -cubeSize);
+            GL.Vertex3(cubeSize, cubeSize, -cubeSize);
+            GL.Vertex3(cubeSize, cubeSize, cubeSize);
+            GL.Vertex3(cubeSize, -cubeSize, cubeSize);
+
+
+            GL.End();
+        }
+
 
         private void displayHelp()
         {
             Console.WriteLine("            MENIU");
             Console.WriteLine("ESC - Parasire aplicatie");
             Console.WriteLine("H - Meniu de ajutor");
+            Console.WriteLine("\n");
+            Console.WriteLine("W - Muta cubul in fata");
+            Console.WriteLine("A - Muta cubul la stanga");
+            Console.WriteLine("S - Muta cubul in spate");
+            Console.WriteLine("D - Muta cubul la dreapta");
+            Console.WriteLine("Q - Muta cubul in jos");
+            Console.WriteLine("E - Muta cubul in sus");
+            Console.WriteLine("\n");
+            Console.WriteLine("R - Reseteaza pozitia cubului");
+
+
         }
     }
 }
